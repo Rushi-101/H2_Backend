547d11936e77fc17a0c71ac2509b8c477dc3d131
Rushikesh J. Metkar <rushijm002@gmail.com>
2020-12-20 18:03:26 +0530

    Profiling!

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,24 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    roll_number = forms.CharField(max_length = 9)
-    room_number = forms.IntegerField()
-    interests = forms.CharField(max_length = 100)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'roll_number','room_number','interests','password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
