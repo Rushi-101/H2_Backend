cc676ba2b3bd4fdbf9ef169d382a26c1e93bbc27
Rushikesh J. Metkar <rushijm002@gmail.com>
2020-12-21 18:03:04 +0530

    Front end changes

--- users/models.py
+++ users/models.py
@@ -5,9 +5,9 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default = 'default.jpg', upload_to = 'profile_pics')
-    roll_no = models.CharField(default = 'None',max_length = 9)
+    roll_no = models.CharField(default = '',max_length = 9)
     room_no = models.IntegerField(default = '0',)
-    interests = models.CharField(default = 'None', max_length = 150)
+    interests = models.CharField(default = '', max_length = 150)
     gc_participation = models.CharField(default = 'None', max_length = 1000)
 
     def __str__(self):
